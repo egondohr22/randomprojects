5c5d6c74ad0d2202d37d86c962ffbc12b03b1906
Egon Dohr <egondohr03@gmail.com>
2022-05-13 08:24:08 +0300

    Done with first part

--- alien_invasion/main.py
+++ alien_invasion/main.py
@@ -21,19 +21,26 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
-
+                self._check_keydown_events(event)
+                self._check_keyup_events(event)
     
-            
+    def _check_keydown_events(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+            elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                self.ship.moving_left = True
+    
+    def _check_keyup_events(self, event):
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                self.ship.moving_right = False
+            elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                self.ship.moving_left = False
+            elif event.key == pygame.K_q:
+                sys.exit()
+
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()

55937f626d7a85f51cc33ad2b1ba01327233ce44
Egon Dohr <egondohr03@gmail.com>
2022-06-03 12:44:47 +0300

    cherno sprite class

--- cherno_game/src/com/onskel/joculet/graphics/Sprite.java
+++ cherno_game/src/com/onskel/joculet/graphics/Sprite.java
@@ -1,5 +1,7 @@
 package com.onskel.joculet.graphics;
 
+import javax.print.attribute.standard.Sides;
+
 public class Sprite {
     
     private final int SIZE;
@@ -10,9 +12,19 @@ public class Sprite {
 
     public Sprite(int size, int x, int y, SpriteSheet sheet) {
         SIZE = size;
+        pixels = new int[SIZE*SIZE];
         this.x = x * size;
         this.y = y * size;
         this.sheet = sheet;
+        load();
+    }
+
+    private void load() {
+        for(int y = 0; y < SIZE; y++) {
+            for(int x = 0; x < SIZE; x++) {
+                pixels[x+y*SIZE] = sheet.pixels[x+this.x + (y + this.y)*sheet.SIZE];
+            }
+        }
     }
 
 }

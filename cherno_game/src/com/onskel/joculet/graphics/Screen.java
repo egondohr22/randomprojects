550462b7cab008fe14164a60ebf23fe4f14d0132
Egon Dohr <egondohr03@gmail.com>
2022-06-03 23:01:56 +0300

    cherno fixed null input

--- cherno_game/src/com/onskel/joculet/graphics/Screen.java
+++ cherno_game/src/com/onskel/joculet/graphics/Screen.java
@@ -34,7 +34,7 @@ public class Screen {
             for(int x = 0; x < width; x++) {
                 int xx = x+xOffset;
                 int tileIndex = ((xx >> 4) & MAP_SIZE_MASK) + ((yy >> 4) & MAP_SIZE_MASK) * MAP_SIZE;
-                pixels[x+y*width] = Sprite.grass.pixels[(x&15) + (y&15)*Sprite.grass.SIZE];
+                pixels[x+y*width] = Sprite.grass.pixels[(xx&15) + (yy&15)*Sprite.grass.SIZE];
             }
         }
     }
--- cherno_game/src/com/onskel/joculet/graphics/SpriteSheet.java
+++ cherno_game/src/com/onskel/joculet/graphics/SpriteSheet.java
@@ -2,13 +2,14 @@ package com.onskel.joculet.graphics;
 
 import java.awt.image.BufferedImage;
 import java.io.Console;
+import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 public class SpriteSheet {
     
-    private String path = "res/textures/spritesheet.png";
+    private String path;
     public final int SIZE;
     public int[] pixels;
 
@@ -23,7 +24,7 @@ public class SpriteSheet {
 
     private void load() {
         try {
-            BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));
+            BufferedImage image = ImageIO.read(new File(path));
             int w = image.getWidth();
             int h = image.getHeight();
             image.getRGB(0, 0, w, h, pixels, 0, w);

14084c3fb295a7e1c4a56c0993e6933f1b7a9b18
Egon Dohr <egondohr03@gmail.com>
2022-06-10 15:37:45 +0300

    cherno tileclass

---
+++ cherno_game/src/com/onskel/joculet/level/tile/Tile.java
@@ -0,0 +1,22 @@
+package com.onskel.joculet.level.tile;
+
+import com.onskel.joculet.graphics.Screen;
+import com.onskel.joculet.graphics.Sprite;
+
+public class Tile {
+    
+    public int x, y;
+    public Sprite sprite;
+
+    public Tile(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    public void render(int x, int y, Screen screen) {
+        
+    }
+
+    public boolean solid() {
+        return false;
+    }
+}

12f99b3c167b3f39787fa2e2ff31c06660b5b33d
Egon Dohr <egondohr03@gmail.com>
2022-06-10 15:54:03 +0300

    cherno grasstile

---
+++ cherno_game/src/com/onskel/joculet/level/tile/GrassTile.java
@@ -0,0 +1,15 @@
+package com.onskel.joculet.level.tile;
+
+import com.onskel.joculet.graphics.Screen;
+import com.onskel.joculet.graphics.Sprite;
+
+public class GrassTile extends Tile {
+
+    public GrassTile(Sprite sprite) {
+        super(sprite);
+    }
+    
+    public void render(int x, int y, Screen screen) {
+        //   
+    }
+}

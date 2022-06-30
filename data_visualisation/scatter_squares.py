0211c1db3d42b73032f1bb814b19f0d79a72faec
Egon Dohr <egondohr03@gmail.com>
2022-06-30 23:38:56 +0300

    datavis

--- data_visualisation/scatter_squares.py
+++ data_visualisation/scatter_squares.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 x_val = []
 y_val = []
 
-for i in range(1, 20):
+for i in range(1, 1000):
     x_val.append(i)
     y_val.append(i*i)
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_val, y_val)
-
+ax.scatter(x_val, y_val, c=y_val, cmap=plt.cm.Blues)
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 ax.tick_params(axis='both', which='major', labelsize=14)
+ax.axis([0, 1100, 0, 1100000])
 plt.show()
